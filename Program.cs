3c6d20f733fa52ba4593d36a402bf5bdbfbe2987
James Ivie <james.ivie@gmail.com>
2021-02-25 11:39:42 -0700

    fixed missing "PackageVersion" update

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace SetVersion
     class Program
     {
         private static Regex VersionNumberValidate = new Regex(@"^[0-9]+(\.[0-9]+){1,3}$", RegexOptions.Compiled);
-        private static Regex ProjectFileVersionReplace = new Regex(@"(?<=<(?:Assembly|File|)Version>)[0-9]+(\.([0-9]+|\*)){1,3}", RegexOptions.Compiled);
+        private static Regex ProjectFileVersionReplace = new Regex(@"(?<=<(?:Assembly|File|Package|)Version>)[0-9]+(\.([0-9]+|\*)){1,3}", RegexOptions.Compiled);
         private static Regex AssemblyInfoVersionReplace = new Regex(@"(?<=\[.*Assembly.*Version ?\(\"")([0-9]+(?:\.(?:[0-9]+|\*)){1,3})", RegexOptions.Compiled);
         static void Main(string[] args)
         {
